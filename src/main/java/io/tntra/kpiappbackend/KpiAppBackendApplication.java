f2e803f63c089972311cb0fdb89123d108a7951c
Jeet Patel <jeet.patel@tntra.io>
2022-12-05 22:05:28 +0530

    Initial commit 🚀

---
+++ src/main/java/io/tntra/kpiappbackend/KpiAppBackendApplication.java
@@ -0,0 +1,13 @@
+package io.tntra.kpiappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpiAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpiAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tntra/kpiappbackend/KpiAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tntra.kpiappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
